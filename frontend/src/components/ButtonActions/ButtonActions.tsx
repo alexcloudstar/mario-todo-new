b5d17b23838b397ed34b90f8471cf5aef98b2153
alexcloudstar <alexcloudstar@gmail.com>
2022-08-28 11:16:41 +0300

    Generate components

---
+++ frontend/src/components/ButtonActions/ButtonActions.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const ButtonActions = () => {
+  return <div>ButtonActions</div>;
+};
+
+export default ButtonActions;
---
+++ frontend/src/components/index.ts
@@ -0,0 +1,7 @@
+export { Navigation } from './Navigation';
+export { Button } from './Button';
+export { List } from './List';
+export { Input } from './Input';
+export { ButtonActions } from './ButtonActions';
+export { Container } from './Container';
+export { AddTodo } from './AddTodo';

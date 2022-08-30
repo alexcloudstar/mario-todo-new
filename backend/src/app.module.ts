a404c12886a97d11fe10857a3f97fe5af7fabe67
alexcloudstar <alexcloudstar@gmail.com>
2022-08-30 12:54:22 +0300

    Generate todos controller, module & service

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,10 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { TodosController } from './todos/todos.controller';
+import { TodosService } from './todos/todos.service';
+import { TodosModule } from './todos/todos.module';
 
 @Module({
-  imports: [UsersModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [UsersModule, TodosModule],
+  controllers: [AppController, TodosController],
+  providers: [AppService, TodosService],
 })
 export class AppModule {}
